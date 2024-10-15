d23f1ee7b7845a25a52d5066acb332b437dca89b
sebaeba2001 <siemadybcia@gmail.com>
2024-10-15 11:56:00 +0200

    Add files via upload

---
+++ 2-IntroToR2/zadanie1.R
@@ -0,0 +1,32 @@
+
+## Zadanie 1: Zapoznanie się z RStudio
+
+**Cel:** Poznanie interfejsu i podstawowych funkcji RStudio.
+
+### Instrukcje i rozwiązanie:
+
+1.  **Uruchom RStudio** na swoim komputerze.
+
+2.  **Zapoznaj się z interfejsem użytkownika:**
+  
+3.  **Stwórz nowy skrypt R:**
+  
+4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
+
+# Source (edytor skryptów) -> do pisania skryptów, a funkcje nie są automatycznie wykonywane
+# Console (Konsola) -> do wyświetlenia uruchomionych funkcji
+# Environment (Środowisko) -> przypisane wartości 
+# Output Panel (Panel Wyjściowy)/more/set as working directory -> zapisanie sciezki zapisu pliku
+
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+
+# Material (miłe, mało nasycone kolory)
+
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
+
+
+  
---
+++ 2-IntroToR2/zadanie2.R
@@ -0,0 +1,38 @@
+## Zadanie 2: Instalacja i użycie pakietów
+
+**Cel:** Nauka instalowania i używania pakietów w R
+
+### Instrukcje i rozwiązanie:
+
+1.  **Zainstaluj pakiet `ggplot2`.**
+  
+  ``` r
+install.packages("ggplot2")
+```
+
+2.  **Załaduj pakiet.**
+  
+  ``` r
+library(ggplot2)
+```
+
+3.  **Sprawdź dokumentację pakietu.**
+  
+  ``` r
+?ggplot2
+```
+
+4.  **Zainstaluj i załaduj** dodatkowy pakiet, który Cię zainteresuje. Listę pakietów w repozytorium CRAN wraz z opisami znajdziesz tutaj: <https://cran.r-project.org/web/packages/available_packages_by_name.html>
+  
+  ``` r
+install.packages("plotly")
+library(plotly)
+```
+
+5.  **Napisz krótki opis** w komentarzu.
+
+``` r
+#pakiet gameR dodaje inspirowane grami gamy kolorów do użycia na wykresach z ggplot2
+```
+
+6.  **Zapisz skrypt** jako `zadanie2.R` i prześlij go do swojego repozytorium.
